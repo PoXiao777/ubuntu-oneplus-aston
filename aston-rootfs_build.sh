2c93d534bd967d6e10a8a6237ae75b71e08ae2ec
PoXiao777 <poxiao676@gmail.com>
2025-04-19 11:10:25 +0800

    Update aston-rootfs_build.sh

--- aston-rootfs_build.sh
+++ aston-rootfs_build.sh
@@ -33,6 +33,9 @@ chroot rootdir apt install -y openssh-server
 chroot rootdir sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 chroot rootdir systemctl enable ssh
 
+chroot rootdir apt install -y wget
+chroot rootdir wget https://github.com/localsend/localsend/releases/download/v1.17.0/LocalSend-1.17.0-linux-arm-64.deb && sudo dpkg -i LocalSend-1.17.0-linux-arm-64.deb; rm -f LocalSend-1.17.0-linux-arm-64.deb
+
 echo "#!/bin/bash
 exit 0" | tee rootdir/var/lib/dpkg/info/python3-defer.postinst
 chroot rootdir dpkg --configure python3-defer
