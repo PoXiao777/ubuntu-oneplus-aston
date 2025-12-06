10f2ad425ba48db3faaa9f17bc96dd88132e49f5
PoXiao777 <poxiao676@gmail.com>
2025-12-06 16:35:23 +0800

    Ubuntu: Update FlClash version to 0.8.90

--- aston-rootfs_build.sh
+++ aston-rootfs_build.sh
@@ -40,7 +40,7 @@ chroot rootdir dpkg -i LocalSend-${LS_VS}-linux-arm-64.deb
 chroot rootdir apt --fix-broken install -y
 chroot rootdir rm -f LocalSend-${LS_VS}-linux-arm-64.deb
 
-FLC_VS="0.8.84"
+FLC_VS="0.8.90"
 chroot rootdir wget https://github.com/chen08209/FlClash/releases/download/v${FLC_VS}/FlClash-${FLC_VS}-linux-arm64.deb
 chroot rootdir dpkg -i FlClash-${FLC_VS}-linux-arm64.deb
 chroot rootdir apt --fix-broken install -y
